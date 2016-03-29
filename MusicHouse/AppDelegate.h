60022872b1680880a82430dc6f0bdb689e3b8b2f
李冬 <lidong@ns.mqcache.net>
2016-03-29 18:56:57 +0800

    Initial Commit

---
+++ MusicHouse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicHouse
+//
+//  Created by 李冬 on 16/3/29.
+//  Copyright © 2016年 Tim Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
